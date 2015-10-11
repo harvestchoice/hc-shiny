503d4961c2c9b8d2968e96a61e77b09ab693036a
Bacou, Melanie (buster) <mel@mbacou.com>
2015-10-11 17:54:51 -0600

    Revert "error"
    This reverts commit ff1f7fcea266d2dc8a766956f9e92d7b5b53a8d6.

---
+++ cell5mValidate/global.R
@@ -0,0 +1,31 @@
+#####################################################################################
+# Title:   HarvestChoice CELL5M Data Validation Tests
+# Date:    July 2015
+# Project: HarvestChoice/IFPRI
+# Authors: Bacou, Melanie <mel@mbacou.com>
+#####################################################################################
+
+setwd("/home/projects/shiny/tmp")
+
+# Load libraries
+library(hcapi3)
+library(shiny)
+
+# Load GAUL admin-2
+g2 <- readRDS("../../cell5m/rdb/g2_2008v09.web.rds")
+g2.dt <- data.table(g2@data)
+g2.dt[, rn := row.names(g2)]
+
+# Key vi
+setkey(vi, varCode)
+
+# Renderer for rhandsometable
+convertNA <- function() htmlwidgets::JS(
+  "function(instance, TD, row, col, prop, value, cellProperties) {
+  if (value === 'NA' | value === 'NaN') {
+  value = '--';
+  Handsontable.renderers.TextRenderer(instance, TD, row, col, prop, value, cellProperties);
+  } else {
+  Handsontable.renderers.NumericRenderer(instance, TD, row, col, prop, value, cellProperties);
+  }
+  }")
